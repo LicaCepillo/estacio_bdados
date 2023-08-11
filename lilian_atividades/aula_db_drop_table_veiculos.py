62ea949c1483e043a875fe4c4343dbad3ed68a64
LicaCepillo <liliancepillo@gmail.com>
2023-08-11 23:03:39 +0100

    comentei a linha DROP  e criei CRATE TABLE Veiculo com os atributos em ordem correta

--- lilian_atividades/aula_db_drop_table_veiculos.py
+++ lilian_atividades/aula_db_drop_table_veiculos.py
@@ -5,9 +5,9 @@ conexao = conector.connect('lilian_atividades/banco_dados_exemplo2.db')
 cursor = conexao.cursor()
 
 # Execução de um comando DROP TABLE
-comando1 = '''DROP TABLE Veiculo;'''
+# comando1 = '''DROP TABLE Veiculo;'''
 
-cursor.execute(comando1)
+# cursor.execute(comando1)
 
 comando2 = '''CREATE TABLE Veiculo (
                 placa CHARACTER(7) NOT NULL,
@@ -18,7 +18,7 @@ comando2 = '''CREATE TABLE Veiculo (
                 marca INTEGER NOT NULL,
                 PRIMARY KEY (placa),
                 FOREIGN KEy (proprietario) REFERENCES pessoa(cpf),
-                FOREIGN KEY (marca) REFEFRNCES Marca(id)
+                FOREIGN KEY (marca) REFERENCES Marca(id)
             );'''
 
 cursor.execute(comando2)
