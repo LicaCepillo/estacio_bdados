6b06aca286f03870686fb13ff5e4f99de5a356bf
LicaCepillo <liliancepillo@gmail.com>
2023-08-14 11:59:57 +0100

    Criação de tabelas par o meu_banco.db (Municipiio, Populacao, Dengue)

---
+++ lilian_atividades/principal_create_table.py
@@ -0,0 +1,42 @@
+import sqlite3 as conector
+
+conexao = None
+cursor = None
+
+try:
+    conexao = conector.connect("lilian_atividades\meu_banco.db")
+    conexao.execute ("PRAGMA foreign_keys = on")
+    cursor = conexao.cursor()
+
+    #comando = '''DROP TABLE Municipio; '''
+    #cursor.execute (comando)
+
+    comando1 = '''CREATE TABLE Municipio(
+                codigo INTEGER NOT NULL,
+                nome VARCHAR(31) NOT NULL,
+                PRIMARY KEY(codigo)
+                );'''
+    cursor.execute(comando1)
+
+    comando2 = '''CREATE TABLE Populacao (
+                codigo INTEGER NOT NULL,
+                ano INTEGER NOT NULL,
+                PRIMARY KEY(codigo, ano),
+                FOREIGN KEY(codigo) REFERENCES Municipio(codigo)
+                );'''
+    cursor.execute(comando2)
+
+    comando3 = '''CREATE TABLE Dengue (
+                codigo INTEGER NOT NULL,
+                ano INTEGER NOT NULL,
+                casos INTEGER NOT NULL,
+                PRIMARY KEY(codigo, ano),
+                FOREIGN KEY(codigo) REFERENCES Municipio(codigo)
+                );'''
+    cursor.execute(comando3)
+
+    conexao.commit()
+except conector.OperationalError as erro:
+    print("Erro Operacional", erro)
+except conector.DatabaseError as erro:
+    print("Erro de Banco de Dados", erro)
